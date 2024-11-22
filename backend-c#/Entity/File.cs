b00458708d24329817876a09eebd0a34683a2c80
Dimonch1k <Dimonch1k@users.noreply.github.com>
2024-11-22 17:51:17 +0200

    TVogjyCh

---
+++ backend-c#/Entity/File.cs
@@ -0,0 +1,20 @@
+﻿namespace backend_c_.Entity
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public int FileSize { get; set; }
+        public string FileType { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime? DeletedAt { get; set; }
+
+        public User User { get; set; }
+        public List<AccessLog> AccessLogs { get; set; }
+        public List<Version> Versions { get; set; }
+        public List<SharedFile> SharedFiles { get; set; }
+    }
+}
---
+++ backend-c#/Entity/Version.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_c_.Entity
+{
+    public class Version
+    {
+        public int Id { get; set; }
+        public int FileId { get; set; }
+        public string VersionName { get; set; }
+        public string VersionPath { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public File File { get; set; }
+    }
+}
