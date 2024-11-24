0da2853d6c79fba2b5fc0a177740c639366a88a8
Dimonch1k <Dimonch1k@users.noreply.github.com>
2024-11-24 18:34:40 +0200

    hOAumi7g: Created access to PostgreSQL DB and AppDbContext

--- backend-c#/Entities/User.cs
+++ backend-c#/Entities/User.cs
@@ -1,18 +1,20 @@
-﻿namespace backend_c_.Entity
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+﻿namespace backend_c_.Entity;
 
-        public List<File> Files { get; set; }
-        public List<AccessLog> AccessLogs { get; set; }
-        public List<SharedFile> OwnedFiles { get; set; }
-        public List<SharedFile> SharedFiles { get; set; }
-    }
+public class User
+{
+  private static readonly List<SharedFile> sharedFiles = new List<SharedFile>();
+
+  public int Id { get; set; }
+  public string Username { get; set; }
+  public string PasswordHash { get; set; }
+  public string Email { get; set; }
+  public string FullName { get; set; }
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+  public List<File> Files { get; set; }
+  public List<AccessLog> AccessLogs { get; set; }
+  public List<SharedFile> OwnedFiles { get; set; } = new();
+  public List<SharedFile> SharedWithFiles { get; set; } = new();
+
 }
